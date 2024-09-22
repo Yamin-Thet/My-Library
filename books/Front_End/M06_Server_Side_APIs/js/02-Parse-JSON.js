65cf545b27f13f38caa4a818882dd54cb33c05f9
daisybb <daisybb545@gmail.com>
2024-09-22 08:42:52 +0630

    added Algorithms and changed module-6

--- books/Front_End/M06_Server_Side_APIs/js/02-Parse-JSON.js
+++ books/Front_End/M06_Server_Side_APIs/js/02-Parse-JSON.js
@@ -1,13 +1,13 @@
-var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?Daisybb2007per_page=5';
+var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5&direction=asc';
 
 fetch(requestUrl)
-  .then(function (response) {
+.then(function (response) {
     return response.json();
-  })
-  .then(function (data) {
+})
+.then(function (data) {
     console.log('Github Repo Issues \n----------');
     for (var i = 0; i < data.length; i++) {
       console.log(data[i].url);
       console.log(data[i].user.login);
     }
-  });
+});
